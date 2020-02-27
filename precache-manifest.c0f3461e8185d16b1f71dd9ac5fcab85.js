ac896d28d68ee67267d5bf4b88507fef11b71cbd
Axel <axosar@gmail.com>
2020-02-27 08:36:44 -0600

    Updates

---
+++ precache-manifest.c0f3461e8185d16b1f71dd9ac5fcab85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dabee63b065efa57b684d9e652f67a6",
+    "url": "/bombeo/index.html"
+  },
+  {
+    "revision": "d7ec174c3726c5f9c855",
+    "url": "/bombeo/static/css/2.28ac8253.chunk.css"
+  },
+  {
+    "revision": "b9cc6dd795a3377fe8eb",
+    "url": "/bombeo/static/css/main.8b66f6fd.chunk.css"
+  },
+  {
+    "revision": "d7ec174c3726c5f9c855",
+    "url": "/bombeo/static/js/2.1e522a5b.chunk.js"
+  },
+  {
+    "revision": "b9cc6dd795a3377fe8eb",
+    "url": "/bombeo/static/js/main.8724286c.chunk.js"
+  },
+  {
+    "revision": "1768958dfe742f6e1e60",
+    "url": "/bombeo/static/js/runtime-main.f43ad4a9.js"
+  },
+  {
+    "revision": "15ca9bfd1b968a1f06ec9ddc79032d46",
+    "url": "/bombeo/static/media/logo-imcyc.15ca9bfd.svg"
+  }
+]);
